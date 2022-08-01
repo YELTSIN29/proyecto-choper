d1679387294e1072532ce0dacc5c885bd1742590
Yeltsin <yeltsinary@gmail.com>
2022-07-31 23:25:12 -0500

    se agrego un tiluto

--- prueba final/listas.py
+++ prueba final/listas.py
@@ -1,5 +1,6 @@
+#este es una prueba
 import random
 carros=['car1','car2','car3','car4']
 for pista in range(4):
       vuelta=random.choice(carros)
-      print(vuelta)
+      print('Gano: ',vuelta)
