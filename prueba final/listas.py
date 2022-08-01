588963d52cf55cb40827b5f4a541e7abe8ae01d3
Yeltsin <yeltsinary@gmail.com>
2022-07-31 23:50:38 -0500

    se agrego un if

--- prueba final/listas.py
+++ prueba final/listas.py
@@ -1,6 +1,12 @@
 #este es una prueba
 import random
+texto=input('¿quien ganara?\n')
+print('---------')
 carros=['car1','car2','car3','car4']
-for pista in range(4):
+for pista in range(1):
       vuelta=random.choice(carros)
       print('Gano: ',vuelta)
+if texto == vuelta:
+      print('Has ganado')
+else:
+      print('Perdistes')
